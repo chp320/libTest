5624e56c257d85f8a5ffdee617cd444a2f283728
chp320 <chp320@naver.com>
2024-09-03 23:30:46 +0900

    Initial commit

---
+++ src/main/java/org/examle/libtest/LibTestApplication.java
@@ -0,0 +1,13 @@
+package org.examle.libtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/examle/libtest/LibTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.examle.libtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
